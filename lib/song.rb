eab5ee136d371a92528909ad8e369a3740df5343
Ally Morson <ally.morson@gmail.com>
2020-01-21 22:31:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,14 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    song = self.name
+    @@all << song
+  end 
+  
+  def self.new_by_name(name)
+    song = self.name 
+  end 
+  
 end
